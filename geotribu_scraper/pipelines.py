10b58fd5e85aa751a6ffa2c716c2db7e63f1cf40
Julien <Guts@users.noreply.github.com>
2020-04-01 17:22:50 +0200

    Use RDP date to name output file

--- geotribu_scraper/pipelines.py
+++ geotribu_scraper/pipelines.py
@@ -28,17 +28,67 @@ folder_output.mkdir(exist_ok=True, parents=True)
 # ######### Pipelines ##############
 # ##################################
 class ScrapyCrawlerPipeline(object):
+    def _isodate_from_raw_dates(
+        self, in_raw_date: str, in_type_date: str = "url"
+    ) -> datetime:
+        """[summary]
+
+        :param list in_dates: [description]
+
+
+        :return: [description]
+        :rtype: datetime
+
+        :example:
+
+        .. code-block:: python
+
+            # here comes an example in Python
+        """
+        # try to convert raw date into a datetime
+        if in_type_date == "url":
+            try:
+                splitted_url = in_raw_date.split("/")[-1]
+                out_date = datetime.strptime(splitted_url, "%Y%m%d")
+                return out_date
+            except Exception as err:
+                logging.error("Raw date parsing {} failed: {}".format(in_raw_date, err))
+                return in_type_date
+        else:
+            raise NotImplementedError
+
     def process_item(self, item: GeoRdpItem, spider):
-        # output filename
-        out_file = folder_output / Path("{}.md".format(item.get("title")))
 
-        # out_item_md = Path(item.get("title"))
-        with out_file.open(mode="w", encoding="UTF8") as out_item_as_md:
-            # out_item_as_md.write("# {}".format(item.get("title")))
-            out_item_as_md.write("# {} \n\n".format(item.get("title")))
-            out_item_as_md.write("{}\n\n----\n\n".format(md(item.get("intro"))))
+        if isinstance(item, GeoRdpItem):
+            rdp_date_raw = item.get("published_date")
+            rdp_date_iso_from_url = item.get("url_full")
+            logging.info("Processing GeoRDP: {}".format(rdp_date_iso_from_url))
 
-        return item
+            if isinstance(
+                self._isodate_from_raw_dates(rdp_date_iso_from_url), datetime
+            ):
+                rdp_date_clean = self._isodate_from_raw_dates(
+                    rdp_date_iso_from_url, in_type_date="url"
+                )
+                logging.info("RDP date clean: " + str(rdp_date_clean.isocalendar()))
+            else:
+                rdp_date_clean = "{} {} {}".format(
+                    rdp_date_raw[0], rdp_date_raw[1], rdp_date_raw[2]
+                )
+                logging.warning(
+                    "Cleaning date failed, using raw date: {}".format(rdp_date_clean)
+                )
+
+            # output filename
+            if isinstance(rdp_date_clean, str):
+                out_file = folder_output / Path("rdp_{}.md".format(rdp_date_clean))
+            elif isinstance(rdp_date_clean, datetime):
+                out_file = folder_output / Path(
+                    "rdp_{}.md".format(rdp_date_clean.strftime("%Y-%m-%d"))
+                )
+            else:
+                pass
+
 
 
 class JsonWriterPipeline(object):
