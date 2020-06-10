d444c754cef14f3a393082ec28425c0b9a0db763
Julien <Guts@users.noreply.github.com>
2020-06-10 08:28:38 +0200

    Use a custom pipeline to store images with names instead of sha1

--- geotribu_scraper/pipelines.py
+++ geotribu_scraper/pipelines.py
@@ -489,3 +489,22 @@ class JsonWriterPipeline(object):
         line = json.dumps(dict(item)) + "\n"
         self.file.write(line)
         return item
+
+
+class CustomImagesPipeline(ImagesPipeline):
+
+    # Name download version
+    def file_path(self, request, response=None, info=None):
+        # item=request.meta['item'] # Like this you can use all from item, not just url.
+        image_guid = request.url.split("/")[-1]
+        return "full/%s" % (image_guid)
+
+    # Name thumbnail version
+    def thumb_path(self, request, thumb_id, response=None, info=None):
+        image_guid = thumb_id + request.url.split("/")[-1]
+        return "thumbs/%s/%s.jpg" % (thumb_id, image_guid)
+
+    # def get_media_requests(self, item, info):
+    #     # yield Request(item['images']) # Adding meta. Dunno how to put it in one line :-)
+    #     for image in item["images"]:
+    #         yield Request(image)
