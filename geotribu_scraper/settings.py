7b6422bf75c2b9785b14f208cde25d5cbcda2a03
Julien M. (In Geo Veritas) <dev@ingeoveritas.com>
2021-12-11 22:59:09 +0100

    ENable at least a base url

--- geotribu_scraper/settings.py
+++ geotribu_scraper/settings.py
@@ -100,7 +100,7 @@ MEDIA_ALLOW_REDIRECTS = True
 
 # -- CUSTOM ---------------------
 # trailing slash is mandatory
-# DEFAULT_URL_BASE = "http://localhost/geotribu_reborn/"
+DEFAULT_URL_BASE = "http://localhost/geotribu_reborn/"
 # DEFAULT_URL_BASE = "https://web.archive.org/web/20170222042705/http://www.geotribu.net/"
 # DEFAULT_URL_BASE = "https://web.archive.org/web/20170222042705/http://www.geotribu.net/"
 
